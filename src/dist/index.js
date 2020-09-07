f8dd9a4a3025a2bdc270a42df3e5c6150ee747d3
lz82 <nickylau82@gmail.com>
2020-09-07 17:45:15 +0800

    :construction: add concurrently

--- src/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var crawler_1 = require("./crawler");
-var hot_search_1 = require("./parser/hot-search");
-var path_1 = require("path");
-var outputPath = path_1["default"].resolve(__dirname, '../output/hot-search.json');
-var hsParser = hot_search_1["default"].getInstance(outputPath);
-var crawler = new crawler_1["default"](hsParser);
-crawler.parseData();
